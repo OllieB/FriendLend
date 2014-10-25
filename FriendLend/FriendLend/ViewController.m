172bbb3050bdd5bef6efbf7dfd1bde51705032ac
Pez Cuckow <email@pezcuckow.com>
2014-10-25 20:58:22 +0100

    Attempt to add ibeacons

--- FriendLend/FriendLend/ViewController.m
+++ FriendLend/FriendLend/ViewController.m
@@ -8,7 +8,13 @@
 
 #import "ViewController.h"
 
+#import "ESTBeaconManager.h"
+
 @interface ViewController ()
+
+@property (nonatomic, strong) ESTBeacon         *beacon;
+@property (nonatomic, strong) ESTBeaconManager  *beaconManager;
+@property (nonatomic, strong) ESTBeaconRegion   *beaconRegion;
 
 @end
 
@@ -17,6 +23,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    /////////////////////////////////////////////////////////////
+    // setup Estimote beacon manager
+    
+    // create manager instance
+    self.beaconManager = [[ESTBeaconManager alloc] init];
+    self.beaconManager.delegate = self;
+    self.beaconManager.avoidUnknownStateBeacons = YES;
+    
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"B9407F30-F5F8-466E-AFF9-25556B57FE6D"];
+    ESTBeaconRegion* region = [[ESTBeaconRegion alloc] initWithProximityUUID:uuid];
+    
+    
+    //self.beaconRegion.notifyOnEntry = self.enterRegionSwitch.isOn;
+    //self.beaconRegion.notifyOnExit = self.exitRegionSwitch.isOn;
+    
+    // start looking for estimote beacons in region
+    // when beacon ranged beaconManager:didEnterRegion:
+    // and beaconManager:didExitRegion: invoked
+    [self.beaconManager startMonitoringForRegion:region];
+    
+    [self.beaconManager requestStateForRegion:region];
+    
+    /////////////////////////////////////////////////////////////
+    // setup view
+    
+    // background
+    
+    /*self.productImage = [[UIImageView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    [self setProductImage];
+    [self.view addSubview:self.productImage];*/
 }
 
 - (void)didReceiveMemoryWarning {
