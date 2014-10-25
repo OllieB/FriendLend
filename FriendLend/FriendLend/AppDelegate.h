b92896b1cba5e3221933a2359205dd788d7696c1
Pez Cuckow <email@pezcuckow.com>
2014-10-25 20:33:20 +0100

    add empty app

---
+++ FriendLend/FriendLend/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FriendLend
+//
+//  Created by Pez Cuckow on 25/10/2014.
+//  Copyright (c) 2014 Pez Cuckow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FriendLend/FriendLend/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FriendLend
+//
+//  Created by Pez Cuckow on 25/10/2014.
+//  Copyright (c) 2014 Pez Cuckow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
