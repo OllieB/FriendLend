4636fd03adf3547d2e39e59a1d20b4570e975216
Pez Cuckow <email@pezcuckow.com>
2014-10-25 23:02:08 +0100

    Add a (broken) view controller

--- FriendLend/FriendLend/OldViewController.h
+++ FriendLend/FriendLend/OldViewController.h
@@ -8,7 +8,8 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MainViewController : UITableViewController
-    @property (strong, nonatomic)  NSArray *menuItems;
+@interface OldViewController : UIViewController
+
+
 @end
 
