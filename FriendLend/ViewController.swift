d1ea895fddefa3da5eec03ea0f98e68844c673d3
Ollie <ollie@Ollie02.hack.com>
2014-10-25 13:20:28 +0100

    Initial Commit

---
+++ FriendLend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendLend
+//
+//  Created by Ollie on 25/10/2014.
+//  Copyright (c) 2014 OllieB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
